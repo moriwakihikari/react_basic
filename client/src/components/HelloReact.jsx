69f9cd862d8c5103aa43f77cfe4f8e0f2a8c8d99
moriwakihikari <daiichidaimandaikichi.gi@gmail.com>
2022-01-12 12:54:49 +0900

    Hello Reactを表示

---
+++ client/src/components/HelloReact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class HelloReact extends React.Component {
+    render() {
+        return (
+            <div className="hello-react">
+                <h1>HelloReact</h1>
+            </div>
+        );
+    }
+}
